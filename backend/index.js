150d77c2837b2c37d88e48cd1d6fb73929e592fc
Divyanshi Rajput <168019864+divyanshi234@users.noreply.github.com>
2026-07-16 17:02:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,15 @@ const cookieParser = require('cookie-parser')
 
 
 const app = express()
-
-app.use(cors({
-    origin:  "https://sopixa-mern-ecommerce.onrender.com",
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://sopixa-mern-ecommerce.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 // app.use(express.json()); 
 
 app.use(cookieParser())
