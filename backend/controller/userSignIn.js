c269a2202991defffc8218f10dc71a1dc6593d26
Divyanshi Rajput <168019864+divyanshi234@users.noreply.github.com>
2026-07-18 14:48:35 +0530

    Update userSignIn.js

--- backend/controller/userSignIn.js
+++ backend/controller/userSignIn.js
@@ -30,7 +30,8 @@ try{
 
         const tokenOption = {
             httpOnly : true,
-            secure : true
+            secure : true,
+             sameSite: "none"
         }
 
         res.cookie("token",token,tokenOption).status(200).json({
